14d0e019f545e155d8ceb85aacf291d936a54d04
baowj-678 <bwj_678@qq.com>
2020-08-25 23:06:43 +0800

    Create Modified_Euler_Method.m

---
+++ Computational Methods/Modified_Euler_Method.m
@@ -0,0 +1,19 @@
+% Author: Bao Wenjie
+% Date  : 2020-8-25
+% Copyright Bao Wenjie
+% Email : bwj_678@qq,com
+% Introduction 改进欧拉法求解常微分方程
+% param func: dy/dx = f(x, y)的函数
+% param y0: 初始值
+% param x0: 初始值
+% param h: 步长
+% parma x0: 待求解点
+
+function y = Modified_Euler_Method(func, y0, x0, h, xn)
+for i=x0+h:h:xn
+    yp = y0 + h*func(x0, y0);
+    yc = y0 + h*func(x0 + h, yp);
+    y0 = (yp + yc)/2;
+end
+y = y0;
+end
